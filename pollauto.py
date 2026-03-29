ea826100dd664aee17f06cdfd923241f328323cf
Gifzbluu <chrishiim524@gmail.com>
2026-03-29 16:41:21 +0800

    URL

--- pollauto.py
+++ pollauto.py
@@ -42,3 +42,19 @@ async def on_ready():
     daily_poll.start()
 
 bot.run(TOKEN)
+
+from flask import Flask
+from threading import Thread
+
+app = Flask("")
+
+@app.route("/")
+def home():
+    return "Bot is alive!"
+
+def run():
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
+
+# 啟動 Web Server 在背景執行
+t = Thread(target=run)
+t.start()
